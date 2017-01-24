3767295bef2908da2675dd4b0778a9cdcce187ec
Mohd Syafiq Azizi <hazelnuts23@gmail.com>
2017-01-24 21:46:39 +0800

    Update

--- index.php
+++ index.php
@@ -3,13 +3,24 @@
     <title>Leaflet JS with OpenWeatherMap and AmCharts</title>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA6_OKikBph5KZMEJR6g1UJ1r1slgMfKLM" async
             defer></script>
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/leaflet/leaflet.css">
+    <link rel="stylesheet" href="css/openweather/owfont-regular.min.css">
+    <link rel="stylesheet" href="css/styles.css">
 
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/leaflet/leaflet.js"></script>
+    <script type="text/javascript" src="js/leaflet-googlemutant/Leaflet.GoogleMutant.js"></script>
+    <script type="text/javascript" src="js/amcharts/amcharts.js"></script>
+    <script type="text/javascript" src="js/amcharts/serial.js"></script>
+    <script type="text/javascript" src="js/amcharts/pie.js"></script>
+    <script type="text/javascript" src="js/amcharts/themes/light.js"></script>
 
 </head>
 <body>
 <div id="mapid"></div>
 <div class="footer">Get source code here: </div>
-<script type="text/javascript" src="js/app.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
 </body>
 </html>
