505571cb183635cbad3b83c0590be13aa1ead153
Mohd Syafiq Azizi bin Rosman <hazelnuts23@gmail.com>
2017-01-24 23:23:03 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -70,7 +70,7 @@ m2.addTo(map);
 function cTof(celsius) {
     var cTemp = celsius;
     var cToFahr = cTemp * 9 / 5 + 32;
-    var message = cTemp + '\xB0C is ' + cToFahr + ' \xB0F.';
+    var message = cTemp + '\xB0C is ' + (cToFahr).toFixed(1) + ' \xB0F.';
     return message;
 }
 
